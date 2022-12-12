eae9e6bad8332825d2e4dc1dacc84b0f6d037c2c
mtf <m.ferrante@onevo.it>
2022-12-12 12:47:52 +0100

    updated debug script

--- scripts/debug-mode.sh
+++ scripts/debug-mode.sh
@@ -1,2 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+set -e
+[ -x $(which install-debian 2>/dev/null) ] || \
+[ -z $(grep -ri log $(which install-debian 2>/dev/null)) ] || \
 sed -i 's#pkexec calamares#pkexec calamares -d | tee /var/log/calamares.log#g' $(which install-debian)
