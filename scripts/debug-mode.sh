33e31fdcf256a7233f05325068e3a15482cb712c
mtf <m.ferrante@onevo.it>
2022-12-12 12:52:11 +0100

    quick fix

--- scripts/debug-mode.sh
+++ scripts/debug-mode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-[ -x $(which install-debian 2>/dev/null) ] || \
-[ -z $(grep -ri log $(which install-debian 2>/dev/null)) ] || \
+[ -x $(which install-debian 2>/dev/null) ] && \
+[ -z $(grep -ri log $(which install-debian 2>/dev/null)) ] && \
 sed -i 's#pkexec calamares#pkexec calamares -d | tee /var/log/calamares.log#g' $(which install-debian)
